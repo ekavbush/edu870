3721df4764f47f2838d21cb3e20b4a47cb67a183
ekavbush <71059096+ekavbush@users.noreply.github.com>
2020-09-10 11:58:34 +0300

    Create ev_script_ya_search.js
    1. Скрипт случайным образом выбирает фразу для поиска из массива фраз.
    2. на сайте Яндекса осуществляет поиск по случайно выбранной фразе. Нажимает "Поиск".
    3. В результатах поиска находит ссылку на Детский Мир и кликает на нее.

---
+++ bot_for_yandex/ev_script_ya_search.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name         Bot for Yandex (ekav)
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  поиск в Яндекс по заданным словам
+// @author       ekav
+// @match        https://yandex.ru/*
+// @grant        none
+// ==/UserScript==
+
+
+let keywords = ['lego duplo','лего дупло детский мир', 'лего на 2 года'];
+let keyword = keywords[getRandom(0,keywords.length)];
+let btnK = document.querySelectorAll(".button_theme_websearch")[0];
+
+if (btnK != undefined){
+    document.getElementById('text').value = keyword;
+    btnK.click();
+}else{
+    let links = document.links;
+
+    for(let i=0; i<links.length; i++){
+        if(links[i].href.indexOf("www.detmir.ru/catalog/index/name/lego_duplo/") != -1){
+            console.log("Ссылка найдена "+links[i]);
+            links[i].click();
+            break;
+        }
+    }
+}
+
+function getRandom(min,max){
+    return Math.floor(Math.random()*(max-min)+min);
+}
